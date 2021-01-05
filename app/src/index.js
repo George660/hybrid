2b149213055aebd6cdbdf64e528ffb2abfe43126
George660 <76912644+George660@users.noreply.github.com>
2021-01-05 23:20:59 +0100

    final commit

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,21 @@
-console.log("Built!");
+const mobileToggle = document.querySelector(".mobile-toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuModal = document.querySelectorAll(".menu-modal");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal-close img");
+
+mobileToggle.addEventListener("click", () => {
+    mobileToggle.classList.toggle("mobile-toggle-zero");
+    mobileToggle.classList.toggle("mobile-toggle-jx");
+    mobileMenu.classList.toggle("mobile-menu-show");
+});
+for (let i = 0; i < menuModal.length; i++) {
+    menuModal[i].addEventListener("click", e => {
+        e.preventDefault();
+        modal.classList.add("modal-show");
+    });
+}
+
+modalCloseBtn.addEventListener("click", () => {
+    modal.classList.remove("modal-show");
+});
